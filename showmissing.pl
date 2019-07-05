3a0f9c1c360baba32a61b12aa339b627bab253b8
rokicki <rokicki@gmail.com>
2019-07-05 15:14:29 -0700

    Updated scan.

--- showmissing.pl
+++ showmissing.pl
@@ -16,18 +16,18 @@ while (<F>) {
 }
 for ($i=0; $i<@v; $i++) {
    next if !$v[$i] ;
-   printf("%2d:", $i) ;
-   for ($j=3; $j<=10; $j++) {
+   printf("%3d:", $i) ;
+   for ($j=3; $j<=14; $j++) {
       if (($v[$i] >> $j) & 1) {
-         $s = sprintf("%d:%d,%d", $j, $e[$i][$j], $d[$i][$j]) ;
-         printf(" %-7s", $s) ;
+         $s = sprintf("%d,%d", $e[$i][$j], $d[$i][$j]) ;
+         printf(" %-5s", $s) ;
          $v[$i] -= 1<<$j ;
          last if $v[$i] == 0 && $d[$i][$j] <= 2 ;
       } elsif (defined($d[$i][$j])) {
-         printf("   %-5d", $d[$i][$j]) ;
+         printf("   %-3d", $d[$i][$j]) ;
          last if $v[$i] == 0 && $d[$i][$j] <= 2 ;
       } else {
-         print "        " ;
+         print "      " ;
       }
    }
    print "\n" ;
