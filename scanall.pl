422a76abb6890eec23d090ef9cd642c74803bcde
rokicki <rokicki@gmail.com>
2019-07-19 21:26:47 -0700

    More updates.

--- scanall.pl
+++ scanall.pl
@@ -1,6 +1,22 @@
+open F, "scanall.log" or die "Can't read scanall.log" ;
+while (<F>) {
+   chomp ;
+   @f = split " ", $_ ;
+   $n = $f[1] ;
+   $k = $f[3] ;
+   $e = $f[9] ;
+   $e[$n][$k] = $e ;
+   $lin[$n][$k] = $_ ;
+}
+close F ;
 for ($n=4; $n<=128; $n++) {
    for ($k=2; $k<$n; $k++) {
       next if ($n & 1) && ($k & 1) ;
-      system("perl scan.pl $n $k") ;
+      next if !defined($e[$n][$k]) ;
+      if ($e[$n][$k] == 0) {
+         print "$lin[$n][$k]\n" ;
+      } else {
+         system("perl scan.pl $n $k") ;
+      }
    }
 }
